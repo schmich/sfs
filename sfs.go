1fc0e251c46c269589ae7bd8e6d7ea84ab8af938
Chris Schmich <schmch@gmail.com>
2016-06-27 02:39:23 -0500

    Add basic command-line app.

---
+++ sfs.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "strconv"
+  "net/http"
+  "path/filepath"
+  "github.com/jawher/mow.cli"
+)
+
+func main() {
+  app := cli.App("sfs", "Static file server - https://github.com/schmich/sfs")
+
+  port := app.IntOpt("p port", 8080, "Listening port")
+  iface := app.StringOpt("i iface interface", "127.0.0.1", "Listening interface")
+  dir := app.StringOpt("d dir directory", ".", "Root directory to serve")
+
+  var err error
+
+  app.Action = func () {
+    *dir, err = filepath.Abs(*dir)
+    if err != nil {
+      panic(err)
+    }
+
+    listen := *iface + ":" + strconv.Itoa(*port)
+
+    fmt.Printf(">>> Serving %s\n", *dir)
+    fmt.Printf(">>> Listening on %s\n", listen)
+
+    server := http.FileServer(http.Dir(*dir))
+    panic(http.ListenAndServe(listen, server))
+  }
+
+  app.Run(os.Args)
+}
