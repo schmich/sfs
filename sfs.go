3c4236aceee7de7c43eabc725e7a4297d50b0898
Chris Schmich <schmch@gmail.com>
2016-07-21 02:24:34 -0500

    Add -g (global) flag to listen on all interfaces.

--- sfs.go
+++ sfs.go
@@ -17,14 +17,19 @@ func main() {
   port := app.IntOpt("p port", 8080, "Listening port")
   iface := app.StringOpt("i iface interface", "127.0.0.1", "Listening interface")
   dir := app.StringOpt("d dir directory", ".", "Directory to serve")
+  allIface := app.BoolOpt("g global", false, "Listen on all interfaces (overrides -i)")
   noBrowser := app.BoolOpt("B no-browser", false, "Do not launch browser")
 
-  var err error
-
   app.Action = func () {
+    var err error
+
     *dir, err = filepath.Abs(*dir)
     if err != nil {
       panic(err)
+    }
+
+    if *allIface {
+      *iface = "0.0.0.0"
     }
 
     portPart := ":" + strconv.Itoa(*port)
